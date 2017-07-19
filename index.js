c6bb74e5d5cfc5b2fcb9a54e53f54c3abd31f542
Rick <nfranc@umail.ucsb.edu>
2017-07-19 15:08:47 -0700

    wha-

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-app.post('/', function(request, response) {
-  response.send('Hello World!')
-})
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('render');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
